bab14b4afb54ec8b6ab697a7dbb444d8349fac37
Lizard-Of-Oz <701551+Lizard-Of-Oz@users.noreply.github.com>
2019-07-12 22:21:10 +0700

    Something wrong happened with commit, fixing

--- Prototype/src/main/java/me/danetnaverno/editoni/minecraft/world/MinecraftBlockState.kt
+++
@@ -1,8 +0,0 @@
-package me.danetnaverno.editoni.minecraft.world
-
-import me.danetnaverno.editoni.common.world.BlockState
-import net.querz.nbt.CompoundTag
-
-open class MinecraftBlockState(properties: CompoundTag) : BlockState(properties)
-
-class UnknownMinecraftBlockState(properties: CompoundTag) : MinecraftBlockState(properties)
--- Prototype/src/main/java/me/danetnaverno/editoni/minecraft/world/MinecraftTileEntity.kt
+++
@@ -1,6 +0,0 @@
-package me.danetnaverno.editoni.minecraft.world
-
-import me.danetnaverno.editoni.common.world.TileEntity
-import net.querz.nbt.CompoundTag
-
-open class MinecraftTileEntity(tag: CompoundTag): TileEntity(tag)
